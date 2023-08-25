73a58e1e7fa0cc857074e94fa8eb21bbf0aa7d54
Kelvin Hoang <>
2023-08-26 00:07:39 +0700

    Implement Network platform.

---
+++ NewsReader/Network/NetworkConstants.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkConstants.swift
+//  NewsReader
+//
+//  Created by MacBook on 25/08/2023.
+//
+
+import Foundation
+
+enum NetworkConstants {
+    static let API_KEY = "cxkAtEZPG4qKNoyJs5icNe4OnQjxVitp"
+    static let API_BASE_URL = "https://api.nytimes.com/svc"
+}
---
+++ NewsReader/Utilities/CancelBag.swift
@@ -0,0 +1,34 @@
+//
+//  CancelBag.swift
+//  NewsReader
+//
+//  Created by MacBook on 25/08/2023.
+//
+
+import Combine
+
+final class CancelBag {
+    fileprivate(set) var subscriptions = Set<AnyCancellable>()
+    
+    func cancel() {
+        subscriptions.removeAll()
+    }
+    
+    func collect(@Builder _ cancellables: () -> [AnyCancellable]) {
+        subscriptions.formUnion(cancellables())
+    }
+    
+    @resultBuilder
+    struct Builder {
+        static func buildBlock(_ cancellables: AnyCancellable...) -> [AnyCancellable] {
+            return cancellables
+        }
+    }
+}
+
+extension AnyCancellable {
+    
+    func store(in cancelBag: CancelBag) {
+        cancelBag.subscriptions.insert(self)
+    }
+}
